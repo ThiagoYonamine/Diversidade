3e904c621ee1283a4110d840b1297d368071fc68
ThiagoYonamine <thyonamine1@gmail.com>
2019-01-31 23:37:46 -0200

    full script

---
+++ Assets/Scripts/ControllerBack.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerBack : MonoBehaviour
+{
+    public GameObject Bia;
+    private Player biaScript;
+    // Start is called before the first frame update
+    void Start()
+    {
+           biaScript= Bia.GetComponent<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(biaScript.getStep()==0){
+            if(PlayerPrefs.GetString("Choice02") == "01-Boat") {
+                biaScript.NextStep(1);
+            } else {
+                biaScript.NextStep(2);
+            }
+            
+        }
+    }
+}
---
+++ Assets/Scripts/ControllerPark.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ControllerPark : MonoBehaviour
+{
+    public GameObject bia;
+    public GameObject[] friends;
+    
+    private bool playing = true;
+    // Update is called once per frame
+    private Player biaScript;
+    void Start() {
+        biaScript= bia.GetComponent<Player>();
+    }
+    void Update()  {
+        if(playing){
+            int count = 0;
+            foreach(GameObject obj in friends) {
+                if(!obj.activeSelf) {
+                    break;
+                } 
+                count++;
+            }
+            if(count == friends.Length){
+               biaScript.NextStep(3);
+               playing = false;
+            } 
+        }
+
+        if(biaScript.getStep() == 5){
+            if(PlayerPrefs.GetString("Choice01") == "01-Boat") {
+                PlayerPrefs.SetString("Choice02", "01-Bridge");
+            } else {
+                PlayerPrefs.SetString("Choice02", "01-Boat");
+            }
+            biaScript.NextStep(6);
+        }
+
+    }
+}
