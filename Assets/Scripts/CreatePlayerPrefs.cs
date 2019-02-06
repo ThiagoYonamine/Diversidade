a0265a76814fd3d634395c79e5f494a17e9054da
ThiagoYonamine <thyonamine1@gmail.com>
2019-02-06 19:07:15 -0200

    final

---
+++ Assets/Scripts/CreatePlayerPrefs.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreatePlayerPrefs : MonoBehaviour
+{
+    public string key;
+    public string value;
+
+    void SaveIfNullPlayerPrefs(){
+        if (!PlayerPrefs.HasKey(key)) {
+            PlayerPrefs.SetString(key, value);
+        }
+    }
+}
